be8946959d48559e6180cfdb2087deb5e2ee7943
London <43586417+FierceBlizzard@users.noreply.github.com>
2021-03-16 18:08:35 -0400

    Added the methods and constructor

--- src/main/java/Case.java
+++ src/main/java/Case.java
@@ -1,3 +1,25 @@
 public class Case {
-    
+    protected String name;
+    protected String color;
+    protected Case(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+
+    protected Case(){
+        this("Sue","Maroon");
+    }
+
+    protected String getOwnerName(){
+        return this.name;
+    }
+
+    protected String getColor(){
+        return this.color;
+    }
+
+    @Override
+    public String toString() {
+        return "Case Owner: "+getOwnerName()+" ,Color: "+getColor();
+    }
 }
