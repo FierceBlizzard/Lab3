5e879d2025382e67d251c2fbffcd0d6d58375a1c
London <43586417+FierceBlizzard@users.noreply.github.com>
2021-03-20 15:23:17 -0400

    Last Case changes. Added a main method and fixed the order of the methods

--- src/main/java/Case.java
+++ src/main/java/Case.java
@@ -1,13 +1,10 @@
 public class Case {
     protected String name;
     protected String color;
+
     protected Case(String name, String color){
         this.name = name;
         this.color = color;
-    }
-
-    protected Case(){
-        this("Sue","Maroon");
     }
 
     protected String getOwnerName(){
@@ -22,4 +19,11 @@ public class Case {
     public String toString() {
         return "Case Owner: "+getOwnerName()+" , Color: "+getColor();
     }
+
+    public static void main(String args[]){
+        Case test1 = new Case("Sue","Maroon");
+        System.out.println("Case Owner: "+test1.getOwnerName()+" Color: "+test1.getColor());
+        System.out.println(test1.toString());
+    }
+
 }
